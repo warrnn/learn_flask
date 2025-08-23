71badfb84acc2759a20260ecd7a9382b513546cb
Bryan Warren <bryanwarren86@gmail.com>
2025-08-23 20:35:33 +0700

    HTML Escape & Routing

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return "<h1>Index Page</h1>"
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
