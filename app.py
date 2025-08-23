10d1d17185d9aaa0cf081ab33de5d6a94df815c8
Bryan Warren <bryanwarren86@gmail.com>
2025-08-23 20:58:51 +0700

    Variable Rules

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return "<h1>Index Page</h1>"
 
-@app.route("/hello")
+@app.route('/hello')
 def hello():
     name = request.args.get("name", "Flask")
     return f"Hello, {escape(name)}!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f"User {escape(username)}"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f"Post {post_id}"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f"Subpath {escape(subpath)}"
